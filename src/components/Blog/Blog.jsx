c3707bb39eacb96eff02c9c5b656d776b28d07c7
Sudipto Das <shiningsudipto@gmail.com>
2023-03-31 10:37:49 +0600

    question component added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import './Blog.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import Question from '../Question/Question';
 
 const Blog = (props) => {
     const { cover, person, name, title, publishdate, readtime } = props.blog;
     const handleBlogTitle = props.handleBlogTitle;
     const handleReadTime = props.handleReadTime;
     return (
+
         <div className='mx-4'>
-            <img className='img-fluid rounded rounded-3 mb-3' src={cover} alt="" />
-            <div className='d-flex'>
-                <img className='blog-avatar me-3 mt-1' src={person} alt="" />
-                <div className='d-flex w-100 align-items-center justify-content-between'>
-                    <div className=''>
-                        <h5>{name}</h5>
-                        <p><small>{publishdate}</small></p>
+            <div>
+                <img className='img-fluid rounded rounded-3 mb-3' src={cover} alt="" />
+                <div className='d-flex'>
+                    <img className='blog-avatar me-3 mt-1' src={person} alt="" />
+                    <div className='d-flex w-100 align-items-center justify-content-between'>
+                        <div className=''>
+                            <h5>{name}</h5>
+                            <p><small>{publishdate}</small></p>
+                        </div>
+                        <p>{readtime} min read  <FontAwesomeIcon onClick={() => handleBlogTitle(title)} className='bookmark' icon={faBookmark} /></p>
                     </div>
-                    <p>{readtime} min read  <FontAwesomeIcon onClick={() => handleBlogTitle(title)} className='bookmark' icon={faBookmark} /></p>
                 </div>
+                <h3>{title}</h3>
+                <button onClick={() => handleReadTime(readtime)} className=' p-0 btn border-0 text-decoration-underline mb-0 blog-read'>Mark as read</button>
+                < hr className='my-4' />
             </div>
-            <h3>{title}</h3>
-            <button onClick={() => handleReadTime(readtime)} className=' p-0 btn border-0 text-decoration-underline mb-0 blog-read'>Mark as read</button>
-            < hr className='my-4' />
+            <div><Question></Question></div>
         </div>
     );
 };
---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Question = () => {
+    return (
+        <div>
+            <div>
+                <h4>What is the difference between Props and State?</h4>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h5>Props</h5>
+                        <p><li>Props are used to pass data from a parent component to a child component.</li></p>
+                    </div>
+                    <div className="col-md-6">
+                        <h5>State</h5>
+                        <p><li>State is used to manage a component's internal state and its behavior.</li></p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h4>How does useState work?</h4>
+            </div>
+            <div>
+                <h4>Which work useEffect do without data load?</h4>
+            </div>
+            <div>
+                <h4>How does React work?</h4>
+            </div>
+
+        </div>
+    );
+};
+
+export default Question;
